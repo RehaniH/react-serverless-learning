467a71e6a215bba2ae315b393d93bddba7cc5700
RehaniH <rehani44perera@gmail.com>
2025-06-22 18:37:00 +1000

    initial changes with navigation

---
+++ src/styled/Global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin:0;
+        font-family: sans-serif;
+        color: #333;
+        font-weight: 300;
+        box-sizing: border-box;
+    }
+
+    h1, h2{
+        margin-bottom: 2rem;
+    }
+    
+
+`;
