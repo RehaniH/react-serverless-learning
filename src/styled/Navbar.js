0acad21bf505aae4abc42f84697ca71934de2c9d
RehaniH <rehani44perera@gmail.com>
2025-06-22 20:41:36 +1000

    style the nav bar and the home page

---
+++ src/styled/Navbar.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import styled from "styled-components";
+
+export const StyledNavbar = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  padding: 20px;
+`;
+
+export const StyledNavBrand = styled.div`
+  font-size: 24px;
+
+  & > a {
+    text-decoration: none;
+  }
+`;
+
+export const StyledNavItems = styled.ul`
+  display: grid;
+  list-style: none;
+  padding-left: 0;
+  grid-gap: 20px;
+  align-items: center;
+  grid-auto-flow: column;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: 200ms;
+
+  &:hover {
+    color: #e16365;
+  }
+`;
