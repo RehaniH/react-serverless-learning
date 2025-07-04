68c6099125b9a9b704dd1c1437ce8bbacf45d30a
RehaniH <rehani44perera@gmail.com>
2025-07-04 13:39:02 +1000

    Implemented the timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -8,18 +8,56 @@ import {
 
 import { Strong } from "../styled/Random";
 
-export default function Game() {
+export default function Game({ history }) {
   const [score, setScore] = useState(1);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS); // count down timer
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setScore((prevScore) => prevScore + 1);
-    }, 1000);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
 
     return () => {
       clearInterval(interval);
     };
-  }, [score]);
+  }, []); //array used to make sure it is run just once
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassed = (endTime.getTime() - startTime.getTime()).toString();
+
+    const formattedMsString = ("0000" + msPassed).slice(-5);
+    // 00000 - first two are the seconds, last three are ms
+
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMsString.substring(0, 2)) - 1;
+
+    const updatedMs =
+      1000 -
+      parseInt(formattedMsString.substring(formattedMsString.length - 3));
+
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+
+    // console.log(formattedMsString.substring(formattedMsString.length - 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = "";
+
+    for (let i = 0; i <= length; i++) {
+      zeros += "0";
+    }
+
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push("/gameOver");
+    }
+  }, [seconds, ms, history]);
 
   return (
     <StyledGame>
@@ -28,7 +66,10 @@ export default function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time:<Strong>00: 000</Strong>
+        Time:
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
