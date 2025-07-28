9e36c5c7d552c16fe5de2f16443e0dbc0a9974d3
RehaniH <rehani44perera@gmail.com>
2025-07-29 08:07:20 +1000

    adding theme to local storage

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+const THEME_STORAGE_NAME = "theme";
 export default () => {
   const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const localStorageTheme = window.localStorage.getItem(THEME_STORAGE_NAME);
+    setTheme(localStorageTheme || "light");
+  }, []);
 
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark");
+      window.localStorage.setItem(THEME_STORAGE_NAME, "dark");
     } else {
       setTheme("light");
+      window.localStorage.setItem(THEME_STORAGE_NAME, "light");
     }
   };
 
